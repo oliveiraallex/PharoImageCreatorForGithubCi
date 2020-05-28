2b46fc369ef1996e726d50e5a90dd4d8c945036c
Allex Oliveira <allex.oliveira@yahoo.com.br>
2020-05-28 02:03:43 +0200

    adjusting the paths

--- imageCreate.sh
+++ imageCreate.sh
@@ -2,13 +2,13 @@
 set -e
 
 #Read the configuration file
-source ../imageConf.sh
-
+source imageConf.sh
+ls -l
 #Download the Pharo image and VM according the configuration file
-for I in "${PHARO[@]}"; do bash environmentCreate.sh "$I" ; done
+for I in "${PHARO[@]}"; do bash PharoImageCreatorForGithubCi/environmentCreate.sh "$I" ; done
 
 # Install the project Pharo script in each image, according configuration file
-for I in "${PHARO[@]}"; do bash projectInstall.sh "$I" ; done
+for I in "${PHARO[@]}"; do bash PharoImageCreatorForGithubCi/projectInstall.sh "$I" ; done
 
 #Create the zip packages with *.image *changes and *.sourcers files 
-for I in "${PHARO[@]}"; do bash packagesCreate.sh "$I" ; done
+for I in "${PHARO[@]}"; do bash PharoImageCreatorForGithubCi/packagesCreate.sh "$I" ; done
